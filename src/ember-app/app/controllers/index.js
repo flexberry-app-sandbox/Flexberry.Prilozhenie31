7538aced7b4a56652f00614556dd56eceafe7d21
Flexberry-man <mail@flexberry.net>
2023-12-13 12:35:02 +0000

    Flexberry code generation

--- src/ember-app/app/controllers/index.js
+++ src/ember-app/app/controllers/index.js
@@ -22,60 +22,61 @@ export default Controller.extend({
             link: 'i-i-s-prilozhenie-3-док-план-за-на-д-l',
             caption: i18n.t('forms.application.sitemap.диспетчеризация-транспортных-средств.i-i-s-prilozhenie-3-док-план-за-на-д-l.caption'),
             title: i18n.t('forms.application.sitemap.диспетчеризация-транспортных-средств.i-i-s-prilozhenie-3-док-план-за-на-д-l.title'),
-            icon: 'tasks',
+            icon: 'chart line',
             children: null
           }, {
             link: 'i-i-s-prilozhenie-3-спр-тип-транс-ср-l',
             caption: i18n.t('forms.application.sitemap.диспетчеризация-транспортных-средств.i-i-s-prilozhenie-3-спр-тип-транс-ср-l.caption'),
             title: i18n.t('forms.application.sitemap.диспетчеризация-транспортных-средств.i-i-s-prilozhenie-3-спр-тип-транс-ср-l.title'),
-            icon: 'chart bar',
+            icon: 'briefcase',
             children: null
           }, {
             link: 'i-i-s-prilozhenie-3-спр-трансп-ср-l',
             caption: i18n.t('forms.application.sitemap.диспетчеризация-транспортных-средств.i-i-s-prilozhenie-3-спр-трансп-ср-l.caption'),
             title: i18n.t('forms.application.sitemap.диспетчеризация-транспортных-средств.i-i-s-prilozhenie-3-спр-трансп-ср-l.title'),
-            icon: 'briefcase',
+            icon: 'building',
             children: null
           }, {
             link: 'i-i-s-prilozhenie-3-спр-номен-l',
             caption: i18n.t('forms.application.sitemap.диспетчеризация-транспортных-средств.i-i-s-prilozhenie-3-спр-номен-l.caption'),
             title: i18n.t('forms.application.sitemap.диспетчеризация-транспортных-средств.i-i-s-prilozhenie-3-спр-номен-l.title'),
-            icon: 'folder',
+            icon: 'building',
             children: null
           }, {
             link: 'i-i-s-prilozhenie-3-спр-контр-аг-l',
             caption: i18n.t('forms.application.sitemap.диспетчеризация-транспортных-средств.i-i-s-prilozhenie-3-спр-контр-аг-l.caption'),
             title: i18n.t('forms.application.sitemap.диспетчеризация-транспортных-средств.i-i-s-prilozhenie-3-спр-контр-аг-l.title'),
+            icon: 'paperclip',
             children: null
           }, {
             link: 'i-i-s-prilozhenie-3-спр-ед-измер-l',
             caption: i18n.t('forms.application.sitemap.диспетчеризация-транспортных-средств.i-i-s-prilozhenie-3-спр-ед-измер-l.caption'),
             title: i18n.t('forms.application.sitemap.диспетчеризация-транспортных-средств.i-i-s-prilozhenie-3-спр-ед-измер-l.title'),
-            icon: 'phone',
+            icon: 'table',
             children: null
           }, {
             link: 'i-i-s-prilozhenie-3-спр-виды-раб-l',
             caption: i18n.t('forms.application.sitemap.диспетчеризация-транспортных-средств.i-i-s-prilozhenie-3-спр-виды-раб-l.caption'),
             title: i18n.t('forms.application.sitemap.диспетчеризация-транспортных-средств.i-i-s-prilozhenie-3-спр-виды-раб-l.title'),
-            icon: 'building',
+            icon: 'list',
             children: null
           }, {
             link: 'i-i-s-prilozhenie-3-спр-объект-стр-l',
             caption: i18n.t('forms.application.sitemap.диспетчеризация-транспортных-средств.i-i-s-prilozhenie-3-спр-объект-стр-l.caption'),
             title: i18n.t('forms.application.sitemap.диспетчеризация-транспортных-средств.i-i-s-prilozhenie-3-спр-объект-стр-l.title'),
-            icon: 'tasks',
+            icon: 'chart bar',
             children: null
           }, {
             link: 'i-i-s-prilozhenie-3-спр-пользов-l',
             caption: i18n.t('forms.application.sitemap.диспетчеризация-транспортных-средств.i-i-s-prilozhenie-3-спр-пользов-l.caption'),
             title: i18n.t('forms.application.sitemap.диспетчеризация-транспортных-средств.i-i-s-prilozhenie-3-спр-пользов-l.title'),
-            icon: 'folder open',
+            icon: 'chart line',
             children: null
           }, {
             link: 'i-i-s-prilozhenie-3-факт-выпол-раб-l',
             caption: i18n.t('forms.application.sitemap.диспетчеризация-транспортных-средств.i-i-s-prilozhenie-3-факт-выпол-раб-l.caption'),
             title: i18n.t('forms.application.sitemap.диспетчеризация-транспортных-средств.i-i-s-prilozhenie-3-факт-выпол-раб-l.title'),
-            icon: 'suitcase',
+            icon: 'table',
             children: null
           }]
         }
